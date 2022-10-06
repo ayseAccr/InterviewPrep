9e6ab27aeadedfc234dd945a980a84e81664cfaa
aysegulacarr <aysegulacar1107@gmail.com>
2022-10-06 18:16:50 +0200

    first commit

---
+++ pkg/BalancedBracket.java
@@ -0,0 +1,40 @@
+
+class Result {
+
+    
+    
+    public static String isBalanced(String s) {
+        Stack<Character>s1=new Stack<>();
+        Stack<Character>s2=new Stack<>();
+        int size=s.length();
+        
+        if(s.length()%2!=0){
+            return "NO";
+        }
+        else{
+          for(int i=0;i<size;i++){
+              if(s.charAt(i)=='(' || s.charAt(i)=='{'|| s.charAt(i)=='[' ){
+                  s1.push(s.charAt(i));
+              }
+              else if(s1.size() == 0){
+            return "NO";
+                  
+              }else if(s.charAt(i)==')' && s1.lastElement()=='(' ){
+                  s1.pop();
+              }else if(s.charAt(i)==']' && s1.lastElement()=='['){
+                  s1.pop();
+              }else if(s.charAt(i)=='}' && s1.lastElement()=='{'){
+                  s1.pop();
+              }
+               
+          } 
+          if(s1.empty()==false){
+              return "NO";
+          }
+          
+        }
+        
+         return  "YES";
+    }
+
+}
